ec3717e2b353a54fb43bc25466ea972e8b98527e
Ofonime Francis <ofonimeusoro01@gmail.com>
2018-05-14 14:58:36 +0100

    [WIP] Working on the landing page of the application

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="header bg-black text-white relative">
+        <div className="nav-bar container mx-auto flex py-4">
+            <nav className="w-2/5">
+            </nav>           
+            <div className="w-1/5 text-center"></div>
+        
+            <div className="w-2/5 flex justify-end">
+              <a className="px-4">Login</a>
+              <a className="bg-white text-black px-2 py-1 rounded-sm">Sign Up</a>
+            </div>
+        </div>
+
+        <div className="search-box absolute">
+            <div className="container mx-auto py-8 px-8 text-white">
+              <div className="tagline_for_search">Taglines goes here</div>
+              <div className="search_form flex">
+                <div className="w-1/2 relative">
+                  <input type="text" className="w-1/2 h-8 px-4 py-2 text-xs" placeholder="Search Business..."/>
+                  <span className="flex item-center absolute pin-r pin-y mb-5"><i className="fa text-grey"></i></span>
+                </div>
+                
+                <div className="w-1/2 relative">
+                  <input type="text" className="inherit important h-8 px-4 py-2 text-xs" placeholder="Location for business" />
+                  <span className="flex item-center absolute pin-r pin-y mb-5"><i className="fa text-grey"></i></span>
+                </div>
+                
+                <button className="bg-black text-white px-2">Search</button>
+              </div>
+              The links will be aligned here
+            </div>
+        </div>
       </div>
     );
   }
